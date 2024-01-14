d5b14146f10732444d6ff67718fa9fb8e6e6bc78
Alexander Nikolayev <nikolayevcode@gmail.com>
2024-01-14 00:21:29 -0500

    Cleaned up gui code, added superclasses :)

--- Kahooot/src/GUI.java
+++ Kahooot/src/GUI.java
@@ -1,25 +1,25 @@
 import javax.swing.*;
 import java.awt.*;
-public class GUI {
-    public static void main(String[] args) {
+public class GUI extends JFrame {
 
-        JFrame frame = new JFrame("KAHOOOT");
-
-        // set frame site
-        int width = 800;
-        int height = 800;
-        frame.setMinimumSize(new Dimension(width, height));
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        public GUI() {
+                super("KAHOOT");
+                int width = 800;
+                int height = 800;
+                setMinimumSize(new Dimension(width, height));
+                setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
 
-        // center the JLabel
-        JLabel lblText = new JLabel("KAHOOOT", SwingConstants.CENTER);
+                // center the JLabel
+                JLabel lblText = new JLabel("KAHOOOT", SwingConstants.CENTER);
 
-        // add JLabel to JFrame
-        frame.getContentPane().add(lblText);
+                // add JLabel to JFrame
+                getContentPane().add(lblText);
+        }
 
-        // display it
-        frame.pack();
-        frame.setVisible(true);
-    }
+        public void display() {
+                pack();
+                setVisible(true);
+        }
 }
+
--- Kahooot/src/Main.java
+++ Kahooot/src/Main.java
@@ -2,6 +2,14 @@ import javax.swing.*;
 import java.awt.*;
 
 public class Main {
-    QuestionDatabase questionDatabase = new QuestionDatabase();
-    Game game = new Game(questionDatabase.getQuestions());
+    public static void main(String[] args) {
+        GUI gui = new GUI();
+        gui.display();
+
+        QuestionDatabase questionDatabase = new QuestionDatabase();
+        Game game = new Game(questionDatabase.getQuestions());
+    }
+
+
+
 }
